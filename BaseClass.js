a95da37826651e54486996b684c367db30e88266
rithiekvaishnev <74966777+rithiekvaishnev@users.noreply.github.com>
2021-02-11 16:47:35 +0530

    Add files via upload

---
+++ BaseClass.js
@@ -0,0 +1,34 @@
+class BaseClass {
+    constructor(x,y, width, height, angle) {
+      var options = {
+        'density':1.5,
+        'friction': 1.0,
+        'restitution':0.5
+      };
+      this.body = Bodies.rectangle(x, y, width, height, options);
+      this.width = width;
+      this.height = height;
+      this.image = loadImage("sprites/base.png");
+      World.add(world, this.body);
+    };
+
+    display(){
+      var pos = this.body.position;
+      //pos.x = mouseX;
+      //pos.y = mouseY;
+      var angle = this.body.angle;
+  
+      push();
+      translate(pos.x, pos.y);
+      rotate(angle);
+      strokeWeight(3);
+      stroke('blue')
+      fill('red')
+      //rectMode(CENTER)
+      //rect(0, 0, this.width, this.height);
+      imageMode(CENTER);
+      image(this.image,0,0,this.width,this.height);
+      pop();
+    };
+  };
+  
